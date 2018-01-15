35d2b00f8d1e4647fed0cc701c4f98f410203091
Justin Johnson <jjohnson@ourfamilywizard.com>
2018-01-15 01:44:04 -0600

    Added comments to current version

--- Blackjack.py
+++ Blackjack.py
@@ -1,15 +1,19 @@
+# import Player class from player.py
 from player import Player
 
+# initiate game by getting name input from user
 playerName = input("What is your name?\n")
+# create Player object for user
 player1 = Player(playerName)
+# game opening dialogue
 print("\nHello " + player1.get_name() + "! Welcome to Blackjack\n")
 print("Dealing cards...")
 print("................\n")
 print("................\n")
 print("................\n")
+# test first_deal method
 player1.first_deal()
 print(player1.get_hand())
-player1.hit()
-print(player1.get_hand())
+# test hit method
 player1.hit()
 print(player1.get_hand())
--- player.py
+++ player.py
@@ -1,23 +1,36 @@
+# import random module
 from random import*
 
+# Player class definition
 class Player:
 
+  # Player class constructor
+  # sets self.name to name parameter
+  # sets self.hand to empty list
   def __init__(self, name):
     self.name = name
     self.hand = []
 
+  # Player first_deal method
+  # store random card values in card1, card2
+  # append card1, card2 to hand list
   def first_deal(self):
     card1 = randint(2, 14)
     card2 = randint(2, 14)
     self.hand.append(card1)
     self.hand.append(card2)
 
+  # Player hit method
+  # store random card value in card
+  # append card to hand list
   def hit(self):
     card = randint(2, 14)
     self.hand.append(card)
 
+  # Player get_name accessor method
   def get_name(self):
     return self.name
 
+  # Player get_hand accessor method
   def get_hand(self):
     return self.hand
